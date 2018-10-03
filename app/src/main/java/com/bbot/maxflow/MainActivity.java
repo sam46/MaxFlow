064fbe070a893763a119106029fb532e200a2888
Sanghamitra1234 <sanghota4567@gmail.com>
2018-10-03 22:35:48 +0530

    Changed the UI of the front page .

--- app/src/main/java/com/bbot/maxflow/MainActivity.java
+++ app/src/main/java/com/bbot/maxflow/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
